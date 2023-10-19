e6fe86a1d90c58a739db6e1891304afc48a34ede
unknown <hfelicesg@fp.insjoaquimmir.cat>
2023-10-19 17:49:00 +0200

    mail de confirmació de registre

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,13 +1,7 @@
 <?php
 
-// use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Log;
-
-
-Route::get('/', function () {
-   Log::info('Loading welcome page');
-   return view('welcome');
-});
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +14,18 @@ Route::get('/', function () {
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
