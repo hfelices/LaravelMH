173ddcf9449c11d0b273a3c21fb9955cd1d0025b
unknown <hfelicesg@fp.insjoaquimmir.cat>
2023-10-24 16:42:22 +0200

    Apartado 7.5

---
+++ laravel/resources/views/mails/testmail.blade.php
@@ -0,0 +1,27 @@
+<!-- <x-mail::message>
+# Introduction
+
+The body of your message.
+
+<x-mail::button :url="''">
+Button Text
+</x-mail::button>
+
+Thanks,<br>
+{{ config('app.name') }}
+</x-mail::message> -->
+@component('mail::message')
+# Hello {{$content['name']}},
+
+
+{{$content['body']}}
+
+
+@component('mail::button', ['url' => $content['url']])
+Click Here
+@endcomponent
+
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\MailController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function (Request $request) {
+    $message = 'Loading welcome page';
+    Log::info($message);
+    $request->session()->flash('info', $message);
     return view('welcome');
 });
+ 
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -27,5 +33,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('mail/test', [MailController::class, 'test']);
+
 
 require __DIR__.'/auth.php';
