bbdb552f101fa950d1f8e301a3ecd2077da1b77c
MarkLopezM <malomo@fp.insjoaquimmir.cat>
2023-11-30 17:45:19 +0100

    6.2

--- laravel/resources/views/components/language-switcher.blade.php
+++ laravel/resources/views/components/language-switcher.blade.php
@@ -1,5 +1,8 @@
 
 <x-dropdown align="right" width="48">
+    <script>
+        const currentLocale = {{ Js::from($currentLocale) }};
+    </script>
     <x-slot name="trigger">
         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
             <div>{{ $availableLocales[$currentLocale] }} ({{ $currentLocale }})</div>
