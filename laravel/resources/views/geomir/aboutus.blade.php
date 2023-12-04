e0675179425f3b826c35c7fe35fbc6c322e6042f
Hector Felices Gonzalez <hfelicesg@fp.insjoaquimmir.cat>
2023-12-04 23:49:39 +0100

    audio y matrix en mouseover

--- laravel/resources/views/geomir/aboutus.blade.php
+++ laravel/resources/views/geomir/aboutus.blade.php
@@ -3,13 +3,19 @@
 @section('content')
 <div class="relative h-screen">
   <!-- Contenido del iframe -->
-  <iframe class="h-full w-full z-0" src='{{ asset("html/matrix.html") }}' frameborder="0"></iframe>
-
+  <canvas id="matrix" class="h-full w-full invisible" style="background: #000; display: block;">
+  </canvas> 
   <!-- Contenido de las cards -->
-  <div class="flex items-center mx-auto justify-center h-full absolute inset-0">
-    <!-- Primera Card -->
+  <div class="flex items-center mx-20 justify-around h-full absolute  inset-0">
+    
+    <div class="max-w-xl bg-white p-4 rounded-md shadow-md" id="imagen">
+      <img  src="https://img.nbc.com/files/images/2013/11/12/dwight-500x500.jpg" alt="Imagen 1" class="mb-4 rounded-md">
+      <h2 class="text-xl font-bold mb-2">Título de la Card 1</h2>
+      <p class="text-gray-700">Descripción de la Card 1.</p>
+      <audio id="audio" src='{{ asset("audio/Rick Astley - Never Gonna Give You Up (Official Music Video).mp3")}}'></audio>
+    </div>
     <div class="max-w-xl bg-white p-4 rounded-md shadow-md">
-      <img src="https://forbes.es/wp-content/uploads/2022/08/Never-Gonna-Give-You-Up-Rick-Astley.jpg" alt="Imagen 1" class="mb-4 rounded-md">
+      <img src="https://www.paperflite.com/sites/default/files/inline-images/tumblr_df754ac3e0c09bc5957dd9b17c08dba8_af889488_400.gif" alt="Imagen 1" class="mb-4 rounded-md">
       <h2 class="text-xl font-bold mb-2">Título de la Card 1</h2>
       <p class="text-gray-700">Descripción de la Card 1.</p>
     </div>
@@ -18,12 +24,74 @@
       <h2 class="text-xl font-bold mb-2">Título de la Card 1</h2>
       <p class="text-gray-700">Descripción de la Card 1.</p>
     </div>
+    
   </div>
 </div>
 
 
-    @include('partials.nav')   
+    @include('partials.nav')
      
+    <script>
+
+
+      document.addEventListener("DOMContentLoaded", function () {
+    var imagen = document.getElementById("imagen");
+    var audio = document.getElementById("audio");
+    var matrix = document.getElementById("matrix");
+
+
+    imagen.addEventListener("mouseover", function () {
+        audio.play();
+        matrix.classList.remove("invisible");
+    });
+
+    // imagen.addEventListener("mouseout", function () {
+    //     audio.pause();
+    //     audio.currentTime = 0; 
+    // });
+
+
+
+    var canvas = document.querySelector('canvas'),
+            ctx = canvas.getContext('2d');
+        
+        // Setting the width and height of the canvas
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        
+        // Setting up the letters
+        var letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ';
+        letters = letters.split('');
+        
+        // Setting up the columns
+        var fontSize = 10,
+            columns = canvas.width / fontSize;
+        
+        // Setting up the drops
+        var drops = [];
+        for (var i = 0; i < columns; i++) {
+          drops[i] = 1;
+        }
+        
+        // Setting up the draw function
+        function draw() {
+          ctx.fillStyle = 'rgba(0, 0, 0, .1)';
+          ctx.fillRect(0, 0, canvas.width, canvas.height);
+          for (var i = 0; i < drops.length; i++) {
+            var text = letters[Math.floor(Math.random() * letters.length)];
+            ctx.fillStyle = '#0f0';
+            ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+            drops[i]++;
+            if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
+              drops[i] = 0;
+            }
+          }
+        }
+        
+        // Loop the animation
+        setInterval(draw, 33);
+});
+  </script>
 @endsection
 
 </x-geomir-layout>
--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -11,10 +11,10 @@
             @include('geomir.places')
         @endif
     </div>
-
+    
     <!-- Div en dispositivos grandes y pequeños -->
     <div class="md:w-5/12 md:block hidden relative">
-        <img class="w-full h-full object-cover z-[-10]" src="{{ asset('img/barcelona_centro.jpg') }}" alt="">
+        <iframe class="w-full h-full object-cover z-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23215.640104711196!2d1.7282362004974146!3d41.23116493697484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zM0nCsDAyJzA1LjQiTiAxwrAzMCczMy41Ilc!5e0!3m2!1sen!2sus!4v1638561261762!5m2!1sen!2sus" width="940" height="350" style="border:0"></iframe>
     </div>
 </div>
     @include('partials.nav')     
