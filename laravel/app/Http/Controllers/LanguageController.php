ef150a2869435f91f01901fbfcff06cde7627977
MarkLopezM <malomo@fp.insjoaquimmir.cat>
2024-01-31 17:18:07 +0100

    creacion rama b1.0

--- laravel/app/Http/Controllers/LanguageController.php
+++ laravel/app/Http/Controllers/LanguageController.php
@@ -15,15 +15,10 @@ class LanguageController extends Controller
            \Log::error("Locale '{$locale}' not exists");
            abort(400);
        }
-
-
-       // Session storage
        $current = \Session::get('locale', $default);
        \Log::debug("Change locale '{$current}' to '{$locale}'");
        \Session::put('locale', $locale);
-       // Set locale
        \App::setLocale($locale);
-       // Go to previous page
        return redirect()->back();
    }
 
