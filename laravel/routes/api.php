2ab85fab5af4fa8793b2c83251abca4bc26b37a2
MarkLopezM <malomo@fp.insjoaquimmir.cat>
2024-01-31 20:04:14 +0100

    intento de tests

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('files',[ FileController::class, 'index']);
+Route::get('files',[ FileController::class, 'show']);
+Route::post('files',[ FileController::class, 'store']);
+Route::put('files',[ FileController::class, 'update']);
+Route::delete('files',[ FileController::class, 'destroy']);
+Route::resource('/api/files', FileController::class);
