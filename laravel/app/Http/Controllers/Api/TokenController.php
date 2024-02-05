994138d9d85f4bd45a27d7a6b529531be6805398
MarkLopezM <malomo@fp.insjoaquimmir.cat>
2024-02-05 20:03:07 +0100

    comienzo punto 2

---
+++ laravel/app/Http/Controllers/Api/TokenController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class TokenController extends Controller
+{
+    public function user(Request $request)
+    {
+        $user = User::where('email', $request->user()->email)->first();
+       
+        return response()->json([
+            "success" => true,
+            "user"    => $request->user(),
+            "roles"   => [$user->role->name],
+        ]);
+    }
+ 
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email'     => 'required|email',
+            'password'  => 'required',
+        ]);
+        if (Auth::attempt($credentials)) {
+            // Get user
+            $user = User::where([
+                ["email", "=", $credentials["email"]]
+            ])->firstOrFail();
+            // Revoke all old tokens
+            $user->tokens()->delete();
+            // Generate new token
+            $token = $user->createToken("authToken")->plainTextToken;
+            // Token response
+            return response()->json([
+                "success"   => true,
+                "authToken" => $token,
+                "tokenType" => "Bearer"
+            ], 200);
+        } else {
+            return response()->json([
+                "success" => false,
+                "message" => "Invalid login credentials"
+            ], 401);
+        }
+    }
+ 
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\FileController;
+use App\Http\Controllers\Api\TokenController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,3 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('files', FileController::class);
+Route::apiResource('users', TokenController::class);
+Route::post('files/{file}', [FileController::class, 'update_workaround']);
